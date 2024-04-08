7927db801569913187c268e8fa24843878e6dfd6
raymart.iphitech <144303488+raymart-iphitech@users.noreply.github.com>
2024-04-08 16:57:46 +0800

    change file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import path from "path"
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -12,5 +13,17 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  resolve: {
+    alias: [
+      { 
+        find: '@', 
+        replacement: path.resolve(__dirname, 'src') 
+      },
+      { 
+        find: '@/public', 
+        replacement: path.resolve(__dirname, 'public') 
+      },
+    ]
   }
 })
